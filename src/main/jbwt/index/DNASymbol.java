8c464d289a75565884fcd08a16bde804ea59e42a
vruano <valentin.ruano@gmail.com>
2017-01-23 13:42:48 -0500

    Improved DNASymbol.valueOf method performance removing the need for large 'switches'

--- src/main/jbwt/index/DNASymbol.java
+++ src/main/jbwt/index/DNASymbol.java
@@ -11,6 +11,19 @@ public enum DNASymbol implements Symbol {
     }, A, C, G, T, R, Y, S, W, K, M, B, D, H, V, N;
 
 
+    private static final DNASymbol[] fromByte = new DNASymbol[256];
+
+    static {
+        for (final DNASymbol symbol : values()) {
+            final int upperCase = symbol.name().charAt(0);
+            assert !Character.isLowerCase(upperCase);
+            assert upperCase < 256;
+            final int lowerCase = Character.toLowerCase(upperCase);
+            assert lowerCase < 256;
+            fromByte[(byte) lowerCase] = fromByte[(byte) upperCase] = symbol;
+        }
+    }
+
     public static final Alphabet<DNASymbol> ALPHABET = new Alphabet<DNASymbol>(DNASymbol.values());
 
     public int toInt() {
@@ -38,37 +51,10 @@ public enum DNASymbol implements Symbol {
     }
 
     public static DNASymbol valueOf(final int c) {
-        if (Character.isUpperCase(c)) {
-            switch (c) {
-                case 'A': return A;
-                case 'C': return C;
-                case 'G': return G;
-                case 'T':
-                case 'U': return T;
-            }
-        }
-        switch (c) {
-                case 'a': return A;
-                case 'c': return C;
-                case 'g': return G;
-                case 't':
-                case 'u': return T;
-                case '$': return $;
-        }
-        switch (Character.toLowerCase(c)) {
-                case 'r': return R;
-                case 'y': return Y;
-                case 's': return S;
-                case 'w': return W;
-                case 'k': return K;
-                case 'm': return M;
-                case 'b': return B;
-                case 'd': return D;
-                case 'h': return H;
-                case 'v': return V;
-                case 'n': return N;
-                default:
-                    throw new IllegalArgumentException("unknown symbol: '" + c + "'");
-        }
+        final DNASymbol result = fromByte[c];
+        if (result == null)
+            throw new IllegalArgumentException("unknown symbol " + c);
+        else
+            return result;
     }
 }
